f3dac08b2901dd18b3d190de07c08f68465af675
David Cuadra Quevedo <dcuadraq@gmail.com>
2015-04-09 19:46:04 -0500

    !st dot-files

---
+++ .fix-mouse.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Default 1.0
+xinput --set-prop "Microsoft Comfort Mouse 3000" "Device Accel Constant Deceleration" 1.0
+# Default 10
+xinput --set-prop "Microsoft Comfort Mouse 3000" "Device Accel Velocity Scaling" 80
